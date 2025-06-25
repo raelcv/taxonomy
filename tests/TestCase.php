211ac7adbdcad1539aa3f1a51ec143b255967e37
raelcv <20374761+raelcv@users.noreply.github.com>
2025-06-25 19:28:32 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,7 @@ namespace Datapec\Taxonomy\Tests;
 
 use BladeUI\Heroicons\BladeHeroiconsServiceProvider;
 use BladeUI\Icons\BladeIconsServiceProvider;
+use Datapec\Taxonomy\TaxonomyServiceProvider;
 use Filament\Actions\ActionsServiceProvider;
 use Filament\FilamentServiceProvider;
 use Filament\Forms\FormsServiceProvider;
@@ -16,7 +17,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Livewire\LivewireServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 use RyanChandler\BladeCaptureDirective\BladeCaptureDirectiveServiceProvider;
-use Datapec\Taxonomy\TaxonomyServiceProvider;
 
 class TestCase extends Orchestra
 {
